07e9b53e75b1cf1a8c39a364d250444828347d0f
Yahya2521 <yahyamos2007@gmail.com>
2025-04-06 15:32:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const navBtn = document.querySelector('#navbar-toggler');
+const navDiv = document.querySelector('.navbar-collapse');
+
+navBtn.addEventListener('click', () => {
+    navDiv.classList.toggle('showNav');
+});
+
+// stoping animation and transition during window resizing //
+let resizeTimer;
+window.addEventLitener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove
+            ('resize-animation-stopper');
+    }, 400);
+})
